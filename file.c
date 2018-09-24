openwrt/uhttpd

commit 77b774b30f9125e77137c57a2ff86de57361d947

build: avoid redefining _DEFAULT_SOURCE
Work around further glibc toolchain annoyances.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

file.c
@@ -17,7 +17,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#define _DEFAULT_SOURCE
+#ifndef _DEFAULT_SOURCE
+# define _DEFAULT_SOURCE
+#define
+
 #define _BSD_SOURCE
 #define _DARWIN_C_SOURCE
 #define _XOPEN_SOURCE 700

main.c
@@ -17,7 +17,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#define _DEFAULT_SOURCE
+#ifndef _DEFAULT_SOURCE
+# define _DEFAULT_SOURCE
+#endif
+
 #define _BSD_SOURCE
 #define _GNU_SOURCE
 #define _XOPEN_SOURCE	700

commit fa5fd45dc36687bb3617a954caa6fe4cc770268c

file: fix CPP syntax error
Fixes: 77b774b ("build: avoid redefining _DEFAULT_SOURCE")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

file.c
@@ -19,7 +19,7 @@
 
 #ifndef _DEFAULT_SOURCE
 # define _DEFAULT_SOURCE
-#define
+#endif
 
 #define _BSD_SOURCE
 #define _DARWIN_C_SOURCE