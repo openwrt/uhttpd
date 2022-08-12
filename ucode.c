openwrt/uhttpd

commit 8cb3f85bdfef35e62f98d7d8b300eaedb6b1b20b

ucode: initialize default library search path
Recent ucode versions require us to explicitly initialize the search path
structure in the parser config.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

ucode.c
@@ -231,6 +231,7 @@ uh_ucode_state_init(struct ucode_prefix *ucode)
 
 	uc_vm_init(vm, &config);
 	uc_stdlib_load(uc_vm_scope_get(vm));
+	uc_search_path_init(&config.module_search_path);
 
 	/* build uhttpd api table */
 	v = ucv_object_new(vm);

commit e3395cd90bed9b7b9fc319e79528fedcc0d947fe

ucode: initialize search path before VM init
Since the VM initialization copies the search path into a runtime array,
we must prepare the path beforehand.

Fixes: 8cb3f85 ("ucode: initialize default library search path")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

ucode.c
@@ -229,9 +229,9 @@ uh_ucode_state_init(struct ucode_prefix *ucode)
 	uc_value_t *v;
 	int exitcode;
 
+	uc_search_path_init(&config.module_search_path);
 	uc_vm_init(vm, &config);
 	uc_stdlib_load(uc_vm_scope_get(vm));
-	uc_search_path_init(&config.module_search_path);
 
 	/* build uhttpd api table */
 	v = ucv_object_new(vm);