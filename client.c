openwrt/uhttpd

commit 15346de8d3ba422002496526ee24c62a3601ab8c

client: Always close connection with request body in case of error
When we run into an error like a 404 Not Found the request body is not
read and will be parsed as part of the next request. The next Request
will then fail because there is unexpected data in it.
When we run into such a problem with a request body close return an
error and close the connection. This should be easier than trying to
recover the state.

We saw this problem when /ubus/ was not installed, but the browser tried
to access it. Then uhttpd returned a 404, but the next request done in
this connection also failed with a HTTP 400, bad request.

Fixes: FS#3378
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

client.c
@@ -138,6 +138,7 @@ void uh_request_done(struct client *cl)
 void __printf(4, 5)
 uh_client_error(struct client *cl, int code, const char *summary, const char *fmt, ...)
 {
+	struct http_request *r = &cl->request;
 	va_list arg;
 
 	uh_http_header(cl, code, summary);
@@ -149,6 +150,17 @@ uh_client_error(struct client *cl, int code, const char *summary, const char *fm
 		va_start(arg, fmt);
 		uh_chunk_vprintf(cl, fmt, arg);
 		va_end(arg);
+	}
+
+	/* Close the connection even when keep alive is set, when it
+	 * contains a request body, as it was not read and we are
+	 * currently out of sync. Without handling this the body will be
+	 * interpreted as part of the next request. The alternative
+	 * would be to read and discard the request body here.
+	 */
+	if (r->transfer_chunked || r->content_length > 0) {
+		cl->state = CLIENT_STATE_CLOSE;
+		cl->request.connection_close = true;
 	}
 
 	uh_request_done(cl);

commit 1e309350fbc0374f6ab6751bf305c51e3675fee4

client: don't advertise keep-alive when sending error replies
Commit 15346de ("client: Always close connection with request body in case
of error") added logic to close keep alive connections on HTTP errors due
to unconsumed request body data.

However, since the check happens after emitting the standard HTTP headers,
uhttpd might incorrectly reply with a `Connection: keep-alive` even if it
is going to close the connection.

Move the check before the emitting of the response headers in order to
ensure that we're sending the correct `Connection: close` line.

Fixes: 15346de ("client: Always close connection with request body in case of error")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

client.c
@@ -141,17 +141,6 @@ uh_client_error(struct client *cl, int code, const char *summary, const char *fm
 	struct http_request *r = &cl->request;
 	va_list arg;
 
-	uh_http_header(cl, code, summary);
-	ustream_printf(cl->us, "Content-Type: text/html\r\n\r\n");
-
-	uh_chunk_printf(cl, "<h1>%s</h1>", summary);
-
-	if (fmt) {
-		va_start(arg, fmt);
-		uh_chunk_vprintf(cl, fmt, arg);
-		va_end(arg);
-	}
-
 	/* Close the connection even when keep alive is set, when it
 	 * contains a request body, as it was not read and we are
 	 * currently out of sync. Without handling this the body will be
@@ -161,6 +150,17 @@ uh_client_error(struct client *cl, int code, const char *summary, const char *fm
 	if (r->transfer_chunked || r->content_length > 0) {
 		cl->state = CLIENT_STATE_CLOSE;
 		cl->request.connection_close = true;
+	}
+
+	uh_http_header(cl, code, summary);
+	ustream_printf(cl->us, "Content-Type: text/html\r\n\r\n");
+
+	uh_chunk_printf(cl, "<h1>%s</h1>", summary);
+
+	if (fmt) {
+		va_start(arg, fmt);
+		uh_chunk_vprintf(cl, fmt, arg);
+		va_end(arg);
 	}
 
 	uh_request_done(cl);