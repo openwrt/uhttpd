openwrt/uhttpd

commit b3e3c05d6781caa5db54fc9913772ddc1af2e045

client: don't send stray EOF chunk on connection timeout
Ensure that any kind of chunk data is only sent when the client connection
is in the body data state in order to avoid sending superfluous `0\r\n\r\n`
chunks on closing idle connections.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

utils.c
@@ -56,7 +56,7 @@ void uh_chunk_vprintf(struct client *cl, const char *format, va_list arg)
 	va_list arg2;
 	int len;
 
-	if (cl->state == CLIENT_STATE_CLEANUP)
+	if (cl->state != CLIENT_STATE_DATA)
 		return;
 
 	uloop_timeout_set(&cl->timeout, conf.network_timeout * 1000);
@@ -91,7 +91,7 @@ void uh_chunk_eof(struct client *cl)
 	if (!uh_use_chunked(cl))
 		return;
 
-	if (cl->state == CLIENT_STATE_CLEANUP)
+	if (cl->state != CLIENT_STATE_DATA)
 		return;
 
 	ustream_printf(cl->us, "0\r\n\r\n");

client.c
@@ -158,10 +158,8 @@ uh_client_error(struct client *cl, int code, const char *summary, const char *fm
 	 * interpreted as part of the next request. The alternative
 	 * would be to read and discard the request body here.
 	 */
-	if (r->transfer_chunked || r->content_length > 0) {
-		cl->state = CLIENT_STATE_CLOSE;
+	if (r->transfer_chunked || r->content_length > 0)
 		cl->request.connection_close = true;
-	}
 
 	uh_request_done(cl);
 }

commit 7e64e8bad2415cec0a6d9770ec379db54273c7a7

client: Allow sending content in DONE state
Allow sending content even when the connection is in done state. When we
run into an error we will be in CLIENT_STATE_DONE state and have to
write some content to the client. Allow writing in normal DATA and in
DONE state.

This fixes http error 403 in LuCI.

Fixes: b3e3c05d6781 ("client: don't send stray EOF chunk on connection timeout")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

utils.c
@@ -56,7 +56,8 @@ void uh_chunk_vprintf(struct client *cl, const char *format, va_list arg)
 	va_list arg2;
 	int len;
 
-	if (cl->state != CLIENT_STATE_DATA)
+	if (cl->state != CLIENT_STATE_DATA &&
+	    cl->state != CLIENT_STATE_DONE)
 		return;
 
 	uloop_timeout_set(&cl->timeout, conf.network_timeout * 1000);
@@ -91,7 +92,8 @@ void uh_chunk_eof(struct client *cl)
 	if (!uh_use_chunked(cl))
 		return;
 
-	if (cl->state != CLIENT_STATE_DATA)
+	if (cl->state != CLIENT_STATE_DATA &&
+	    cl->state != CLIENT_STATE_DONE)
 		return;
 
 	ustream_printf(cl->us, "0\r\n\r\n");